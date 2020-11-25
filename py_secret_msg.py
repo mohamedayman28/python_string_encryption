d06c60fa61328cb1593836686d1a59864cf7b6a2
Mohamed Ayman <34320712+mohamedayman28@users.noreply.github.com>
2020-11-25 23:34:43 +0200

    Convert as a command-line tool.
    * Enhance code readability.
    * Rename from py_secret_msg to message_encryption

--- py_secret_msg.py
+++ py_secret_msg.py
@@ -1,11 +1,34 @@
-#!/usr/bin/env python3.6
+class MessageEncryption:
+    """ Allow user to encrypt their messages or decrypt it on demand. Along as,
+    allow them to choice between to kind of algorithms Shift and Matrix.
 
-class SecretMessage:
-    def __init__(self, msg):
-        self.msg = msg
+    Note: The Matrix algorithm in progress, more on that in the README.md.
+    """
 
-    def shift_encryption(self, encrypted=False):
-        """ Method to shift every letter by 3 letter."""
+    # User inputs along with help messages.
+    msg = input('Your Message: ')
+
+    # Select encryption type.
+    help_msg = 'Shift Algorithm type "s".\nMatrix Algorithm type "m".\nNumbers and empty choice will be considered as "s".'
+    print('\n' + help_msg)
+    encryption_type = input('\n' + 'Encryption Type: ')
+
+    # Select to decrypt or encrypt message.
+    help_msg = 'To encrypt your message type "e", for decryption type "d", numbers and empty choice will be considered as "e".'
+    print('\n' + help_msg)
+    decrypt_or_encrypt = input('\n' + 'Your choice goes here: ')
+    # On decrypt option.
+    if decrypt_or_encrypt == 'd':
+        encrypted = True
+    else:
+        encrypted = False
+
+    def shift_encryption(self, msg=msg, encrypted=encrypted):
+        """ Shift every letter by 3 letters.
+        args:
+            * msg: The user input message.
+            * encrypted: Check if message is encrypted(default=True).
+        """
         encrypted_msg = ''
 
         upper_letters = [
@@ -17,10 +40,9 @@ class SecretMessage:
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
         ]
 
-        for letter in self.msg:
-
+        for letter in msg:
             steps = 3
-            # If message is encrypted already.
+            # If user select to decrypt.
             if encrypted:
                 # decrypt it.
                 steps = -3
@@ -39,7 +61,17 @@ class SecretMessage:
                 crypting = (index + steps) % 26
                 encrypted_msg += lower_letters[crypting]
             else:
-                # Add else characters as it is (ex: !!).
+                # Add else characters as it is (ex: !!, ' ').
                 encrypted_msg += letter
 
-        return encrypted_msg
+        print('\n' + f'Here is your secret message {encrypted_msg}')
+
+    def matrix_encryption(self):
+        print('\n' + 'Sorry, the Matrix Algorithm is in progress.')
+
+    # If user select the matrix algorithm.
+    if encryption_type == 'm':
+        matrix_encryption(None)
+    # For shift algorithm encryption_type == 's'.
+    else:
+        shift_encryption(None)
